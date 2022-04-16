d3c9679f7d183085cd0f9a197d20780a02a4f72e
AnhQuanTrl <anhquan.tranle0601@gmail.com>
2022-04-16 18:32:19 +0700

    chore: add purging css to remove unwanted rule

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("scss/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("scss/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
